7a55aa111ba3c9c69fa2037b375c2f700b4ef9f0
motaher <motaher@revesoft.com>
2020-09-21 00:34:17 +0600

    fi

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,10 +1,11 @@
 package sfgpetclinic;
 
+import guru.springframework.sfgpetclinic.SfgPetClinicApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = SpringRunner.class)
+@SpringBootTest(classes = {SfgPetClinicApplication.class})
 class SfgPetClinicApplicationTests {
 
     @Test
