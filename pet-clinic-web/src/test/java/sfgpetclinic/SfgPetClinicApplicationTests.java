f70454eed97339491fb4cea9a39ac23859e9d662
motaher <motaher@revesoft.com>
2020-09-21 00:10:26 +0600

    adding the release plugin

--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,8 +2,9 @@ package sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringRunner.class)
 class SfgPetClinicApplicationTests {
 
     @Test
