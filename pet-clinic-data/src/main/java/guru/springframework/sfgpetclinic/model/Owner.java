652ffba0b54513205a4a50c49e1c709722d23748
motaher <motaher@revesoft.com>
2020-11-11 20:41:18 +0600

    added properties, closes #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -6,11 +6,39 @@ public class Owner extends Person{
 
     private Set<Pet> pets;
 
+    private String address;
+    private String city;
+    private String telePhone;
+
     public Set<Pet> getPets() {
         return pets;
     }
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelePhone() {
+        return telePhone;
+    }
+
+    public void setTelePhone(String telePhone) {
+        this.telePhone = telePhone;
     }
 }
